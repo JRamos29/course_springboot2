6f7f656b4d25aa333ced874318c23667a5b23224
JRamos29 <malebolge@gmail.com>
2022-05-25 17:44:33 -0300

    OrderStatus enum

--- src/main/java/com/jrammos29/course/entities/Order.java
+++ src/main/java/com/jrammos29/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jrammos29.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -25,7 +26,7 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
-//	private Integer orderStatus;
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -40,11 +41,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
-//		setOrderStatus(orderStatus);
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -64,15 +65,15 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 	
-//	public OrderStatus getOrderStatus() {
-//		return OrderStatus.valueOf(orderStatus);
-//	}
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
 
-//	public void setOrderStatus(OrderStatus orderStatus) {
-//		if (orderStatus != null) {
-//			this.orderStatus = orderStatus.getCode();
-//		}
-//	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	public User getClient() {
 		return client;

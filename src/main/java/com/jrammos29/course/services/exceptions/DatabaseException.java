9bfd1dfbce45675eef16c21d39b0173e3f9c4864
JRamos29 <malebolge@gmail.com>
2022-05-26 09:35:20 -0300

    Exception handling - delete

--- src/main/java/com/jrammos29/course/services/exceptions/DatabaseException.java
+++ src/main/java/com/jrammos29/course/services/exceptions/DatabaseException.java
@@ -1,10 +1,10 @@
-//package com.jrammos29.course.services.exceptions;
-//
-//public class DatabaseException extends RuntimeException {
-//
-//	private static final long serialVersionUID = 1L;
-//
-//	public DatabaseException(String msg) {
-//		super(msg);
-//	}
-//}
+package com.jrammos29.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
